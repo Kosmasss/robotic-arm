fdca8f2db81d5af576ab078b088ada2bb172b7da
kosmas <53818687+Kosmasss@users.noreply.github.com>
2024-06-29 18:33:01 +0300

    Update Robotic_arm.ino

--- Robotic_arm.ino
+++ Robotic_arm.ino
@@ -1,39 +1,68 @@
 #include <Servo.h>
 
-Servo myServo;
-Servo myServo1;
-Servo myServo2;
-const int buttonPin = 2
-const int xPin = A0;        // the number of the analog pin for X-axis
-const int yPin = A1;
 
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+
+const int joystickXPin = A0;
+const int joystickYPin = A1;
+const int buttonPin = 6;
+const int servo1Pin = 9;
+const int servo2Pin = 10;
+const int servo3Pin = 11;
+
+
+int servo1Pos = 90;
+int servo2Pos = 90;
+int servo3Pos = 90;
+
+
+bool buttonPressed = false;
 
 void setup() {
-  myServo.attach(9);
-  myServo1.attach(10);
-  myServo2.attach(11);
-  int buttonState = 0 ;
+  
+  servo1.attach(servo1Pin);
+  servo2.attach(servo2Pin);
+  servo3.attach(servo3Pin);
+
+  
+  servo1.write(servo1Pos);
+  servo2.write(servo2Pos);
+  servo3.write(servo3Pos);
+
+  
   pinMode(buttonPin, INPUT);
-  }
-
-
+  Serial.begin(9600);
+}
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
-
-  int xValue = analogRead(xPin);  // read the X-axis value
-  int yValue = analogRead(yPin);  // read the Y-axis value
   
-  int servoAngleX = map(xValue, 0, 1023, 0, 180);  // map X-axis value to servo angle
-  int servoAngleY = map(yValue, 0, 1023, 0, 180);  // map Y-axis value to servo angle
+  int joystickXValue = analogRead(joystickXPin);
+  int joystickYValue = analogRead(joystickYPin);
 
 
-   if (buttonState == HIGH) {
-    // If the button is pressed, move the second servo based on the X-axis value:
-    myServo2.write(servoAngleX);
-  } else {
-    // If the button is not pressed, move the first servo based on the X-axis value:
-    myServo1.write(servoAngleX);  // move servo based on X-axis value
-    delay(5);  // delay for smoother movement
+  servo1Pos = map(joystickXValue, 0, 1023, 0, 180);
+  servo2Pos = map(joystickYValue, 0, 1023, 0, 180);
+
+  
+  servo1.write(servo1Pos);
+  servo2.write(servo2Pos);
+
+  
+  if (digitalRead(buttonPin) == HIGH && !buttonPressed) {
+    buttonPressed = true;
+    // Κίνηση του τρίτου σερβο
+    servo3Pos = (servo3Pos == 90) ? 0 : 90; 
+    servo3.write(servo3Pos);
   }
+
+ 
+  if (digitalRead(buttonPin) == LOW && buttonPressed) {
+    buttonPressed = false;
+  }
+
+  
+  delay(15);
 }
